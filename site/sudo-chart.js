1fce9e31dc802bdb3e0e41e44dc7194ff32df137
Christopher Guan <chris.guan@cguan-03.workdayinternal.com>
2015-11-13 13:38:31 -0800

    fixed tree view. Issue was related with ID's

--- site/sudo-chart.js
+++ site/sudo-chart.js
@@ -2,7 +2,7 @@
 
 function truncate(str, limit) { var bits, i; if ("string" !== typeof str) { return ''; } bits = str.split(''); if (bits.length > limit) { for (i = bits.length - 1; i > -1; --i) { if (i > limit) { bits.length = i; } else if (' ' === bits[i]) { bits.length = i; break; } } bits.push('...'); } return bits.join(''); }
 
-
+var xxx;
 
 var margin = {top: 5, right: 120, bottom: 20, left: 250},
 width = 1150 - margin.right - margin.left,
@@ -10,117 +10,141 @@ height = 700 - margin.top - margin.bottom;
 
 $(document).ready(function(){
   sudo.storage.start();
-$.ajax({
-  type: "GET",
-  url: "https://i-06f25fdf.workdaysuv.com/ccx/internalapi/reporting/v1/super/dataSource/",
-  dataType: 'json',
-  async: true,
-  headers: {
-    "Authorization": "Bearer 7cs4m24vzic2zl1phvg036ucz"
-  },
-  success: function (data){
-    $.each(data.data, function (index, data) {
-      var d = {
-        "wid": data.id,
-        "label": data.descriptor,
-        "categories": [],
-        "object": data.primaryBusinessObject
-      };
-      $.each(data.dataSourceCategories || [], function(index, data){
-        d.categories.push(data.descriptor);
-      });
-      sudo.storage.put("s", data.id, d);
-    });
-  }
-});
 
-$("#search").keyup(function () {
-  var out = ""
-  $.each(sudo.search.query($("#search").val()), function (index, data) {
-    console.log(data);
-    var item = sudo.storage.get(data.split("$")[0], data.split("$")[1]);
-    out += "<li><a id='" + data + "' class='bo' href='#'>" + item.label + "</a></li>";
+  $.ajax({
+    type: "GET",
+    url: "https://i-06f25fdf.workdaysuv.com/ccx/internalapi/reporting/v1/super/dataSource/",
+    dataType: 'json',
+    async: true,
+    headers: {
+      "Authorization": "Bearer 7cs4m24vzic2zl1phvg036ucz"
+    },
+    success: function (data){
+      $.each(data.data, function (index, data) {
+        var d = {
+          "wid": data.id,
+          "label": data.descriptor,
+          "categories": [],
+          "object": data.primaryBusinessObject
+        };
+        $.each(data.dataSourceCategories || [], function(index, data){
+          d.categories.push(data.descriptor);
+        });
+        sudo.storage.put("s", data.id, d);
+      });
+    }
   });
-  $("#source-list").html(out);
-  $(".bo").click(function(e){
-    var data = $(e.target).attr("id");
-    var item = sudo.storage.get(data.split("$")[0], data.split("$")[1]);
-    loadBusinessObject(item.object.id)
+
+  $("#search").keyup(function () {
+    var out = ""
+    $.each(sudo.search.query($("#search").val()), function (index, data) {
+      //console.log(data);
+      var item = sudo.storage.get(data.split("$")[0], data.split("$")[1]);
+      out += "<li><a id='" + data + "' class='bo' href='#'>" + item.label + "</a></li>";
+    });
+    $("#source-list").html(out);
+    $(".bo").click(function(e){
+      var data = $(e.target).attr("id");
+      var item = sudo.storage.get(data.split("$")[0], data.split("$")[1]);
+      loadBusinessObject(item.object.id)
+    });
   });
-});
 
   function loadBusinessObject(businessObjectWID) {
-  $.ajax({
-  type: "GET",
-  url: "https://i-06f25fdf.workdaysuv.com/ccx/internalapi/reporting/v1/super/businessObject/" + businessObjectWID,
-  dataType: 'json',
-  async: true,
-  headers: {
-    "Authorization": "Bearer 7cs4m24vzic2zl1phvg036ucz"
-  },
-  success: function (data){
-      data.name = data.descriptor;
-      data.children = data.classReportFields;
- 
-      $.each(data.children, function (index, data) {
-        data.name = truncate(data.descriptor, 27); 
-      });
-      root = data;
-      root.x0 = height / 2;
-      root.y0 = 300;
+    $.ajax({
+      type: "GET",
+      url: "https://i-06f25fdf.workdaysuv.com/ccx/internalapi/reporting/v1/super/businessObject/" + businessObjectWID,
+      dataType: 'json',
+      async: true,
+      headers: {
+        "Authorization": "Bearer 7cs4m24vzic2zl1phvg036ucz"
+      },
+      success: function (levela){
+        levela.name = levela.descriptor;
+        levela.children = levela.classReportFields;
+        var reqs = [];
+        $.each(levela.children, function (index, levelb) {
+          levelb.name = truncate(levelb.descriptor, 27);
+          if (levelb.relatedBusinessObject && levelb._children == null && levelb.children == null) {
+            console.log("https://i-06f25fdf.workdaysuv.com/ccx/internalapi/reporting/v1/super/businessObject/" + levelb.relatedBusinessObject.id);
+            reqs.push($.ajax({
+              type: "GET",
+              url: "https://i-06f25fdf.workdaysuv.com/ccx/internalapi/reporting/v1/super/businessObject/" + levelb.relatedBusinessObject.id,
+              dataType: 'json',
+              async: true,
+              headers: {
+                "Authorization": "Bearer 7cs4m24vzic2zl1phvg036ucz"
+              },
+              success: function (levelc){
+                levelb.children = levelc.classReportFields;
+                $.each(levelb.children, function (index, leveld) {
+                  leveld.name = leveld.descriptor;
+                });
+              }, error: function (e){
+                console.log(e);
+              }
+            }));
+          }
+        });
 
-      function collapse(d) {
-        if (d.children) {
-          d._children = d.children; 
-          d._children.forEach(collapse); 
-          d.children = null;
-        } 
+        $.when.apply($,reqs).always(function(){
+          console.log("DONE");
+          root = levela;
+          console.log(root);
+  //         root = {
+  //  "name": "analytics",
+  //  "children": [
+  //   {
+  //    "name": "cluster",
+  //    "children": [
+  //     {"name": "AgglomerativeCluster", "size": 3938},
+  //     {"name": "CommunityStructure", "size": 3812},
+  //     {"name": "HierarchicalCluster", "size": 6714},
+  //     {"name": "MergeEdge", "size": 743}
+  //    ]
+  //   },
+  //   {
+  //    "name": "graph",
+  //    "children": [
+  //     {"name": "BetweennessCentrality", "size": 3534},
+  //     {"name": "LinkDistance", "size": 5731},
+  //     {"name": "MaxFlowMinCut", "size": 7840},
+  //     {"name": "ShortestPaths", "size": 5914},
+  //     {"name": "SpanningTree", "size": 3416}
+  //    ]
+  //   },
+  //   {
+  //    "name": "optimization",
+  //    "children": [
+  //     {"name": "AspectRatioBanker", "size": 7074}
+  //    ]
+  //   }
+  //  ]
+  // };
+
+
+
+
+          xxx = root;
+          root.x0 = height / 2;
+          root.y0 = 300;
+
+          function collapse(d) {
+            if (d.children) {
+              d._children = d.children; 
+              d._children.forEach(collapse); 
+              d.children = null;
+            } 
+          }
+
+          root.children.forEach(collapse);
+          update(root);
+        });
       }
-
-      root.children.forEach(collapse);
-      update(root); 
+    });
   }
-});
-}
 
-function loadSubBusinessObject(businessObjectWID) {
-  console.log(businessObjectWID);
-
-  $.ajax({
-  type: "GET",
-  url: "https://i-06f25fdf.workdaysuv.com/ccx/internalapi/reporting/v1/super/businessObject/" + businessObjectWID,
-  dataType: 'json',
-  async: true,
-  headers: {
-    "Authorization": "Bearer 7cs4m24vzic2zl1phvg036ucz"
-  },
-  success: function (data){
-      console.log("success");
-      data.name = data.descriptor;
-      data.children = data.classReportFields;
- 
-      $.each(data.children, function (index, data) {
-        data.name = truncate(data.descriptor, 27); 
-      });
-
-      root.x0 = height / 2;
-      root.y0 = 300;
-
-      function collapse(d) {
-        if (d.children) {
-          d._children = d.children; 
-          d._children.forEach(collapse);
-          d.children = null;
-        }
-      }
-
-      root.children.forEach(collapse);
-      update(root);
-  }
-});
-}
-
-  var duration = 750,i = 0,
+  var duration = 750, i = 0,
   root;
 
 
@@ -138,132 +162,197 @@ function loadSubBusinessObject(businessObjectWID) {
 
 //d3.select(self.frameElement).style("height", "400px");
 
-function update(source) {
+// function update(source) {
 
-  // Compute the new tree layout.
-  var nodes = tree.nodes(root).reverse(),
-  links = tree.links(nodes);
+//   // Compute the new tree layout.
+//   var nodes = tree.nodes(root).reverse(),
+//   links = tree.links(nodes);
 
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 250; });
+//   // Normalize for fixed-depth.
+//   nodes.forEach(function(d) { d.y = d.depth * 180; });
 
-  // Update the nodes…
-  var node = svg.selectAll("g.node")
-  .data(nodes, function(d) { return d.id || (d.id = ++i); });
+//   // Update the nodes…
+//   var node = svg.selectAll("g.node")
+//   .data(nodes, function(d) { return d.id || (d.id = ++i); });
 
-  // Enter any new nodes at the parent's previous position.
-  var nodeEnter = node.enter().append("g")
-  .attr("class", "node")
-  .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-  .on("click", click);
+//   // Enter any new nodes at the parent's previous position.
+//   var nodeEnter = node.enter().append("g")
+//   .attr("class", "node")
+//   .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+//   .on("click", click);
 
-  nodeEnter.append("circle")
-  .attr("r", 1e-6)
-  .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+//   nodeEnter.append("circle")
+//   .attr("r", 1e-6)
+//   .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
 
-  nodeEnter.append("text")
-  .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-  .attr("dy", ".35em")
-  .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-  .text(function(d) { return d.name; })
-  .style("fill-opacity", 1e-6).style("font-size", "15px");
+//   nodeEnter.append("text")
+//   .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+//   .attr("dy", ".35em")
+//   .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+//   .text(function(d) { return d.name; })
+//   .style("fill-opacity", 1e-6).style("font-size", "15px");
 
-  // Transition nodes to their new position.
-  var nodeUpdate = node.transition()
-  .duration(duration)
-  .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+//   // Transition nodes to their new position.
+//   var nodeUpdate = node.transition()
+//   .duration(duration)
+//   .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
-  nodeUpdate.select("circle")
-  .attr("r", 4.5)
-  .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+//   nodeUpdate.select("circle")
+//   .attr("r", 4.5)
+//   .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
 
-  nodeUpdate.select("text")
-  .style("fill-opacity", 1);
+//   nodeUpdate.select("text")
+//   .style("fill-opacity", 1);
 
-  // Transition exiting nodes to the parent's new position.
-  var nodeExit = node.exit().transition()
-  .duration(duration)
-  .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-  .remove();
+//   // Transition exiting nodes to the parent's new position.
+//   var nodeExit = node.exit().transition()
+//   .duration(duration)
+//   .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+//   .remove();
 
-  nodeExit.select("circle")
-  .attr("r", 1e-6);
+//   nodeExit.select("circle")
+//   .attr("r", 1e-6);
 
-  nodeExit.select("text")
-  .style("fill-opacity", 1e-6);
+//   nodeExit.select("text")
+//   .style("fill-opacity", 1e-6);
 
-  // Update the links…
-  var link = svg.selectAll("path.link")
-  .data(links, function(d) { return d.target.id; });
+//   // Update the links…
+//   var link = svg.selectAll("path.link")
+//   .data(links, function(d) { return d.target.id; });
 
-  // Enter any new links at the parent's previous position.
-  link.enter().insert("path", "g")
-  .attr("class", "link")
-  .attr("d", function(d) {
-    var o = {x: source.x0, y: source.y0};
-    return diagonal({source: o, target: o});
-  });
+//   // Enter any new links at the parent's previous position.
+//   link.enter().insert("path", "g")
+//   .attr("class", "link")
+//   .attr("d", function(d) {
+//     var o = {x: source.x0, y: source.y0};
+//     return diagonal({source: o, target: o});
+//   });
 
-  // Transition links to their new position.
-  link.transition()
-  .duration(duration)
-  .attr("d", diagonal);
+//   // Transition links to their new position.
+//   link.transition()
+//   .duration(duration)
+//   .attr("d", diagonal);
 
-  // Transition exiting nodes to the parent's new position.
-  link.exit().transition()
-  .duration(duration)
-  .attr("d", function(d) {
-    var o = {x: source.x, y: source.y};
-    return diagonal({source: o, target: o});
-  })
-  .remove();
+//   // Transition exiting nodes to the parent's new position.
+//   link.exit().transition()
+//   .duration(duration)
+//   .attr("d", function(d) {
+//     var o = {x: source.x, y: source.y};
+//     return diagonal({source: o, target: o});
+//   })
+//   .remove();
 
-  // Stash the old positions for transition.
-  nodes.forEach(function(d) {
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
-}
-
-// Toggle children on click.
-function click(d) {
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else {
-    
-    if (d.relatedBusinessObject && d._children == null) {
+//   // Stash the old positions for transition.
+//   nodes.forEach(function(d) {
+//     d.x0 = d.x;
+//     d.y0 = d.y;
+//   });
+// }
 
 
-      $.ajax({
-        type: "GET",
-        url: "https://i-06f25fdf.workdaysuv.com/ccx/internalapi/reporting/v1/super/businessObject/" + d.relatedBusinessObject.id,
-        dataType: 'json',
-        async: true,
-        headers: {
-          "Authorization": "Bearer 7cs4m24vzic2zl1phvg036ucz"
-        },
-        success: function (data){
-            d.children = data.classReportFields;
-     
-            $.each(d.children, function (index, data) {
-              data.name = data.descriptor; 
-            });
+  function update(source) {
 
-        }
-      });
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root).reverse(),
+    links = tree.links(nodes);
 
-  } else {
-    d.children = d._children;
-    d._children = null;
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+    // Update the nodes…
+    var node = svg.selectAll("g.node")
+    .data(nodes, function(d) { return d.KEY || (d.KEY = ++i); });
+
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+    .on("click", click);
+
+    nodeEnter.append("circle")
+    .attr("r", 1e-6)
+    .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeEnter.append("text")
+    .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+    .attr("dy", ".35em")
+    .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+    .text(function(d) { return d.name; })
+    .style("fill-opacity", 1e-6);
+
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition()
+    .duration(duration)
+    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+    nodeUpdate.select("circle")
+    .attr("r", 4.5)
+    .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeUpdate.select("text")
+    .style("fill-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    var nodeExit = node.exit().transition()
+    .duration(duration)
+    .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+    .remove();
+
+    nodeExit.select("circle")
+    .attr("r", 1e-6);
+
+    nodeExit.select("text")
+    .style("fill-opacity", 1e-6);
+
+    // Update the links…
+    var link = svg.selectAll("path.link")
+    .data(links, function(d) { return d.target.KEY; });
+
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("path", "g")
+    .attr("class", "link")
+    .attr("d", function(d) {
+      var o = {x: source.x0, y: source.y0};
+      return diagonal({source: o, target: o});
+    });
+
+    // Transition links to their new position.
+    link.transition()
+    .duration(duration)
+    .attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+    .duration(duration)
+    .attr("d", function(d) {
+      var o = {x: source.x, y: source.y};
+      return diagonal({source: o, target: o});
+    })
+    .remove();
+
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
   }
 
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    }  else {
+      d.children = d._children;
+      d._children = null;
+    }
+    update(d);
   }
-
-  update(root);
-  
-
-}
-
 
 });
+
+
+
+
+
+
